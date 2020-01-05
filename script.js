75191d1b04c930852cec09e2f2733d32ab22f194
Shirleeeeeeey <56938286+Shirleeeeeeey@users.noreply.github.com>
2020-01-05 08:24:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+function g1(){
+	document.getElementById("g1").style.display="block";
+}
+function g2(){
+	document.getElementById("g2").style.display="block";
+}
+function g3(){
+	document.getElementById("g3").style.display="block";
+}
+function g4(){
+	document.getElementById("g4").style.display="block";
+}
+function g5(){
+	document.getElementById("g5").style.display="block";
+}
+function g6(){
+	document.getElementById("g6").style.display="block";
+}
